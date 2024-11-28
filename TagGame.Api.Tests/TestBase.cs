05df95325e49abffb2b2e1ea034ffdac8006b65c
fabian <famabiker@gmail.com>
2024-11-28 13:58:14 +0100

    impl db test container

--- TagGame.Api.Tests/TestBase.cs
+++ TagGame.Api.Tests/TestBase.cs
@@ -2,17 +2,44 @@ global using AutoFixture;
 global using FluentAssertions;
 global using Moq;
 global using Xunit;
+using Testcontainers.PostgreSql;
 
-namespace TagGameApi.Tests;
+namespace TagGame.Api.Tests;
 
-public class TestBase
+public class TestBase : IAsyncLifetime
 {
     private protected Fixture _fixture;
-
+    private protected PostgreSqlContainer? _dbContainer;
+    
     public TestBase()
     {
         _fixture = new Fixture();
         var customization = new SupportMutableValueTypesCustomization();
         _fixture.Customize(customization);
+    }
+
+    void UseDbTestContainer()
+    {
+        _dbContainer = new PostgreSqlBuilder()
+            .WithDatabase("TestDb")
+            .WithUsername("testuser")
+            .WithPassword("testpassword")
+            .Build();
+    }
+
+    public async Task InitializeAsync()
+    {
+        if (_dbContainer is null)
+            return;
+        
+        await _dbContainer.StartAsync();
+        Environment.SetEnvironmentVariable("ConnectionStrings:DefaultConnection", _dbContainer.GetConnectionString());
+    }
+
+    public async Task DisposeAsync()
+    {
+        if (_dbContainer is null)
+            return;
+        await _dbContainer.DisposeAsync();
     }
 }
