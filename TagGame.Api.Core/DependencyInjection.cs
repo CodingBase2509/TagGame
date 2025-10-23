0acdb5cdbb2dc78986adb7023bac12935860037d
fabian <famabiker@gmail.com>
2025-10-23 14:20:34 +0200

    add folder structure to solution + add centralized package management - close #42 #18

--- TagGame.Api.Core/DependencyInjection.cs
+++ TagGame.Api.Core/DependencyInjection.cs
@@ -1,15 +1,10 @@
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-
 namespace TagGame.Api.Core;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddCore(this IServiceCollection services, IConfiguration configuration)
-    {
+    public static IServiceCollection AddCore(this IServiceCollection services, IConfiguration configuration) =>
         // Placeholder for Core registration (EF Core, Repositories, Feature Services, Validators, Mapping)
         // Intentionally minimal for v2 skeleton to satisfy DI wiring and build.
-        return services;
-    }
+        services;
 }
 
